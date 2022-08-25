81558284db6a730c3e85b36f6a1dccf3d1fb6756
William Lowrimore <wlowrimore@gmail.com>
2022-08-25 18:31:28 -0500

    setup and beginning Navbar

---
+++ components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='fixed w-full h-20 shadow-xl z-[100]'>
+      <div>
+
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
