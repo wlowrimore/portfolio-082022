b05898a48d7261600856e84a193472afc68e0033
William Lowrimore <wlowrimore@gmail.com>
2022-08-25 20:28:11 -0500

    creates and adds navbar logo

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import Image from 'next/image'
 
 const Navbar = () => {
   return (
     <div className='fixed w-full h-20 shadow-xl z-[100]'>
-      <div>
-
+      <div className='flex justify-between items-center w-full h-full px-2 2xl:px16'>
+        <Image src="/../public/assets/navLogo.png" alt="/" width="175" height="175" />
       </div>
     </div>
   )
