92204eeb3eaf928bdd4e163b382df20621758149
William Lowrimore <wlowrimore@gmail.com>
2022-08-26 13:16:36 -0500

    completes basic About Component

--- components/Main.jsx
+++ components/Main.jsx
@@ -16,10 +16,10 @@ const Main = () => {
           </h1>
           <h1 className="py-2 text-gray-700">A Software Developer</h1>
           <p className="py-4 text-gray-600 max-w-[70%] m-auto">
-            I'm a Certified Full-Stack Web Developer with a focus on front-end
-            web design. I'm currently employeed with Coroutine as a Junior
-            Software developer, while continuing to broaden my knowledge of
-            back-end technologies.
+            I'm a Graduate of Vanderblt University's Full-Stack Coding Bootcamp. I'm 
+            currently employeed as a Junior Software Developer where I focus on front-end 
+            web development. I also continue to study full-stack best practices, as I further
+            my knowledge and skills in the latest software development technologies.
           </p>
           <div className="flex items-center justify-between max-w-[330px] m-auto py-4">
             <div className="rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300">
