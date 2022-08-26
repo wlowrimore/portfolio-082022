9f03e16cd88f9d89c4862c5858dc876f3032c897
William Lowrimore <wlowrimore@gmail.com>
2022-08-26 15:49:58 -0500

    completes skills page...mostly

---
+++ components/Skills.jsx
@@ -0,0 +1,212 @@
+import React from "react";
+import Image from "next/image";
+
+const Skills = () => {
+  return (
+    <div className="w-full lg:h-screen p-2">
+      <div className="max-w=[1240px] mx-auto flex flex-col justify-center h-full">
+        <p className="uppercase text-xl tracking-widest text-[#5651e5]">
+          Skills
+        </p>
+        <h2 className="py-4">What I Can Do</h2>
+        <div className="grid md;grid-cols-2 lg:grid-cols-4 gap-8">
+          <div className="p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300">
+            <div className="grid grid-cols-2 gap-4 justify-center items-center">
+              <div className="m-auto">
+                <Image
+                  src="/../public/assets/skills/html5_logo.png"
+                  width="64px"
+                  height="64px"
+                  alt="/"
+                />
+              </div>
+              <div className="flex flex-col items-center justify-center">
+                <h3>HTML</h3>
+              </div>
+            </div>
+          </div>
+
+          <div className="p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300">
+            <div className="grid grid-cols-2 gap-4 justify-center items-center">
+              <div className="m-auto">
+                <Image
+                  src="/../public/assets/skills/css_logo.png"
+                  width="64px"
+                  height="64px"
+                  alt="/"
+                />
+              </div>
+              <div className="flex flex-col items-center justify-center">
+                <h3>CSS</h3>
+              </div>
+            </div>
+          </div>
+
+          <div className="p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300">
+            <div className="grid grid-cols-2 gap-4 justify-center items-center">
+              <div className="m-auto">
+                <Image
+                  src="/../public/assets/skills/bootstrap_logo.png"
+                  width="70px"
+                  height="64px"
+                  alt="/"
+                />
+              </div>
+              <div className="flex flex-col items-center justify-center">
+                <h3>Bootstrap 5</h3>
+              </div>
+            </div>
+          </div>
+
+          <div className="p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300">
+            <div className="grid grid-cols-2 gap-4 justify-center items-center">
+              <div className="m-auto">
+                <Image
+                  src="/../public/assets/skills/tailwind_logo.png"
+                  width="75px"
+                  height="64px"
+                  alt="/"
+                />
+              </div>
+              <div className="flex flex-col items-center justify-center">
+                <h3>Tailwind</h3>
+              </div>
+            </div>
+          </div>
+
+          <div className="p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300">
+            <div className="grid grid-cols-2 gap-4 justify-center items-center">
+              <div className="m-auto">
+                <Image
+                  src="/../public/assets/skills/gimp_logo.png"
+                  width="75px"
+                  height="68px"
+                  alt="/"
+                />
+              </div>
+              <div className="flex flex-col items-center justify-center">
+                <h3>Gimp</h3>
+              </div>
+            </div>
+          </div>
+
+          <div className="p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300">
+            <div className="grid grid-cols-2 gap-4 justify-center items-center">
+              <div className="m-auto">
+                <Image
+                  className="rounded-lg"
+                  src="/../public/assets/skills/JS_logo.png"
+                  width="64px"
+                  height="64px"
+                  alt="/"
+                />
+              </div>
+              <div className="flex flex-col items-center justify-center">
+                <h3>JavaScript</h3>
+              </div>
+            </div>
+          </div>
+
+          <div className="p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300">
+            <div className="grid grid-cols-2 gap-4 justify-center items-center">
+              <div className="m-auto">
+                <Image
+                  src="/../public/assets/skills/rubyOnRails_logo.png"
+                  width="64px"
+                  height="64px"
+                  alt="/"
+                />
+              </div>
+              <div className="flex flex-col items-center justify-center">
+                <h3>Ruby on Rails</h3>
+              </div>
+            </div>
+          </div>
+
+          <div className="p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300">
+            <div className="grid grid-cols-2 gap-4 justify-center items-center">
+              <div className="m-auto">
+                <Image
+                  src="/../public/assets/skills/react_logo.png"
+                  width="68px"
+                  height="64px"
+                  alt="/"
+                />
+              </div>
+              <div className="flex flex-col items-center justify-center">
+                <h3>React.js</h3>
+              </div>
+            </div>
+          </div>
+
+          <div className="p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300">
+            <div className="grid grid-cols-2 gap-4 justify-center items-center">
+              <div className="m-auto">
+                <Image
+                  className="rounded-lg"
+                  src="/../public/assets/skills/handlebars_logo.png"
+                  width="64px"
+                  height="64px"
+                  alt="/"
+                />
+              </div>
+              <div className="flex flex-col items-center justify-center">
+                <h3>Handlebars</h3>
+              </div>
+            </div>
+          </div>
+
+          <div className="p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300">
+            <div className="grid grid-cols-2 gap-4 justify-center items-center">
+              <div className="m-auto">
+                <Image
+                  src="/../public/assets/skills/nodejs_logo.png"
+                  width="68px"
+                  height="68px"
+                  alt="/"
+                />
+              </div>
+              <div className="flex flex-col items-center justify-center">
+                <h3>Node.js</h3>
+              </div>
+            </div>
+          </div>
+
+          <div className="p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300">
+            <div className="grid grid-cols-2 gap-4 justify-center items-center">
+              <div className="m-auto">
+                <Image
+                  src="/../public/assets/skills/nextjs_logo.png"
+                  width="75px"
+                  height="68px"
+                  alt="/"
+                />
+              </div>
+              <div className="flex flex-col items-center justify-center">
+                <h3>Next.js</h3>
+              </div>
+            </div>
+          </div>
+
+          <div className="p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300">
+            <div className="grid grid-cols-2 gap-4 justify-center items-center">
+              <div className="m-auto">
+                <Image
+                  src="/../public/assets/skills/github_logo.png"
+                  width="64px"
+                  height="64px"
+                  alt="/"
+                />
+              </div>
+              <div className="flex flex-col items-center justify-center">
+                <h3>Github</h3>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Skills;
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,9 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import Navbar from '../components/Navbar'
-import Main from '../components/Main'
-import About from '../components/About'
+import Head from "next/head";
+import Image from "next/image";
+import Navbar from "../components/Navbar";
+import Main from "../components/Main";
+import About from "../components/About";
+import Skills from "../components/Skills";
 
 export default function Home() {
   return (
@@ -15,7 +16,7 @@ export default function Home() {
       <Navbar />
       <Main />
       <About />
-      
+      <Skills />
     </div>
-  )
+  );
 }
